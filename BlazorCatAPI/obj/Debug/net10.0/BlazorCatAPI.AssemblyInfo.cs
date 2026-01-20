2e8f196e38dbc1aa3a81e874e7af10309dcfeff6
KindredMouse <b2004tl@gmail.com>
2026-01-20 11:11:46 -0600

    no api update

--- BlazorCatAPI/obj/Debug/net10.0/BlazorCatAPI.AssemblyInfo.cs
+++ BlazorCatAPI/obj/Debug/net10.0/BlazorCatAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorCatAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d608c76b0ad8e3bc4b0c6c0472b4bf8fb80f3b73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f5c89384a38060dfba8001610a1da593d52b9b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorCatAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorCatAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
